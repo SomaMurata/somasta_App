3e5ef43533a26f0abd02ffb0f692135cfce22d0c
somamurata <101445039+SomaMurata@users.noreply.github.com>
2022-04-19 21:32:54 +0900

    coding finished

--- App.jsx
+++ App.jsx
@@ -1,23 +1,41 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import firebase from 'firebase';
+// import React from 'react';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      {/* eslint-disable-next-line */}
-      <StatusBar style="auto" />
-    </View>
-  );
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       {/* eslint-disable-next-line */}
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+import React from 'react'
+import { Provider } from 'react-redux'
+import { createAppContainer } from 'react-navigation'
+import AppNavigator from './AppNavigator'
+import configureStore from './configureStore'
+
+const store = configureStore()
+
+const AppContainer = createAppContainer(AppNavigator)
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <AppContainer />
+      </Provider>
+    )
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ modules/firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAdrjAd2hjH-WG73mUdSEq-CnvtdlzM5oM',
+  authDomain: 'somastaapp.firebaseapp.com',
+  projectId: 'somastaapp',
+  storageBucket: 'somastaapp.appspot.com',
+  messagingSenderId: '338262940970',
+  appId: '1:338262940970:web:dd50f2694e36c8c0271253',
+};
